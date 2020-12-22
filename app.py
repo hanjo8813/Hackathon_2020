e3bf310d4b29009daa949bbe7ac8eee7de400888
Han Jae Won <71180414+hanjo8813@users.noreply.github.com>
2020-12-23 00:46:09 +0900

    .

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def mypage():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 #---------------------------------------------------------
 
     
